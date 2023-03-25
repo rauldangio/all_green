1b1f27f512e472d06bf5dc7f1d850effb8c5f34c
rauldangio <jojofagqwrt@gmail.com>
2023-03-25 04:09:40 -0300

    testando

---
+++ generator.py
@@ -0,0 +1,30 @@
+import os
+import random
+
+start = 0
+end = 45000
+
+def get_date():
+    day = random.randint(1,31)
+    mouth = random.randint(1,12)
+    year = random.randint(2022,2023)
+    date = f'{year}/{mouth}/{day}'
+
+    return date
+
+
+def make_commit(date):
+    while start <= 45000:
+        os.system(f"echo {date} > file.txt")
+        os.system("git add file.txt")
+        os.system(f"git commit -m 'commit' --date={date}")
+    
+
+def main():
+    date = get_date()
+    make_commit(date)
+    os.system('git push origin master')
+
+
+if __name__ == '__main__':
+    main()
